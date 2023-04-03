6b26cbf5ce66402416961618d2690d80e74c21b4
git_Vickyprinz <kariukivictor315@gmail.com>
2023-04-03 13:58:15 +0300

    updated

--- config/application.rb
+++ config/application.rb
@@ -37,10 +37,11 @@ module Api
 
     config.middleware.insert_before 0, Rack::Cors do
       allow do
-        origins 'http://localhost:3001', 'https://food-carty-api.onrender.com'
+        origins 'http://localhost:3001', 'https://food-carty-api.onrender.com', 'https://admirable-pie-7df123.netlify.app'
         resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
       end
     end
+    
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
